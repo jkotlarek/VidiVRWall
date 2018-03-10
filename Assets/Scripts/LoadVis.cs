ae9582304e55ef4fcc80026807ddeb696c38026d
DESKTOP-VSCBR3L\Joe <jtkotlarek@gmail.com>
2018-03-09 23:03:56 -0800

    Resize html canvases for vis's Move d3 files again

---
+++ Assets/Scripts/LoadVis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZenFulcrum.EmbeddedBrowser;
+
+public class LoadVis : MonoBehaviour
+{
+
+    public string visName;
+
+    string fullpath;
+
+    // Use this for initialization
+    void Start()
+    {
+        fullpath = Application.streamingAssetsPath + "/D3Vis/" + visName + ".html";
+        gameObject.GetComponent<Browser>().LoadURL(fullpath, true);
+    }
+
+}
