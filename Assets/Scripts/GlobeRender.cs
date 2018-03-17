f4427a376f24c1435fbcb6c22c02f9ddd1ee95fd
DESKTOP-VSCBR3L\Joe <jtkotlarek@gmail.com>
2018-03-17 01:30:43 -0700

    Globe and 2D map textures, materials and objects. Meta files for everything

---
+++ Assets/Scripts/GlobeRender.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobeRender : MonoBehaviour {
+
+    List<Point> points;
+    bool updateQueued;
+
+	// Use this for initialization
+	void Start () {
+        points = new List<Point>();
+        updateQueued = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //render pointerinos
+
+        updateQueued = false;
+	}
+
+    public void AddPoint(Point newPoint)
+    {
+        points.Add(newPoint);
+        updateQueued = true;
+    }
+
+    public void ClearPoints()
+    {
+        points.Clear();
+        updateQueued = true;
+    }
+
+    public void AddPoints(List<Point> newPoints)
+    {
+        points.AddRange(newPoints);
+        updateQueued = true;
+    }
+    
+}
+
+public class Point
+{
+    public Vector2 Position { get; set; }
+    public Color Color { get; set; }
+    public float Radius { get; set; }
+
+    public Point()
+    {
+        Position = new Vector2();
+        Color = new Color();
+        Radius = 0.01f;
+    }
+
+    public Point(Vector2 position, Color color, float radius)
+    {
+        Position = position;
+        Color = color;
+        Radius = radius;
+    }
+}
