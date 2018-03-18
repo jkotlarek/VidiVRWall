98b36afc2f9f4451452895e788b17b8abc970aee
DESKTOP-VSCBR3L\Joe <jtkotlarek@gmail.com>
2018-03-18 05:33:31 -0700

    added csv loader added globe point renderer (in progress)

---
+++ Assets/Scripts/CSVHelper.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+/// <summary>
+/// Represents a single row of data from CSV file
+/// </summary>
+public class Event
+{
+    public Dictionary<string, string> values;
+
+    public Event()
+    {
+        values = new Dictionary<string, string>();
+    }
+}
+
+/// <summary>
+/// Parses CSV file
+/// </summary>
+public class CSVHelper : List<string[]>
+{
+    protected string csv = string.Empty;
+    protected string separator = ",";
+
+    public CSVHelper(string csv, string separator = ",")
+    {
+        this.csv = csv;
+        this.separator = separator;
+
+        foreach (string line in Regex.Split(csv, "\n").ToList().Where(s => !string.IsNullOrEmpty(s)))
+        {
+            string[] values = Regex.Split(line, separator);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                //Trim values
+                values[i] = values[i].Trim('\"');
+            }
+
+            this.Add(values);
+        }
+    }
+
+    public static string Load(string path)
+    {
+        return File.ReadAllText(path);
+    }
+}
+
+/// <summary>
+/// Loads File from path
+/// </summary>
+public static class CSVReader
+{
+
+    public static List<Event> Read(string file)
+    {
+        string fullpath = Application.streamingAssetsPath + file;
+
+        var csv = new CSVHelper(CSVHelper.Load(fullpath), "\t");
+
+        List<Event> data = new List<Event>();
+        string[] header = csv[0];
+        foreach (string[] line in csv)
+        {
+            if (line == header) continue;
+            var d = new Event();
+
+            for (int i = 0; i < header.Length; i++)
+            {
+                d.values.Add(header[i], line[i + 1]); //i+1 because the data has an extra column that we're craftily ignoring hehe
+            }
+
+            data.Add(d);
+        }
+
+        return data;
+    }
+}
--- Assets/Scripts/GlobeRender.cs
+++ Assets/Scripts/GlobeRender.cs
@@ -1,40 +1,115 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GlobeRender : MonoBehaviour {
 
+    
+    List<Event> data;
     List<Point> points;
+    List<GameObject> pointObjects;
+
     bool updateQueued;
+    string filename = "/D3Vis/data/terrorism_small.tsv";
+    //string filename = "/D3Vis/data/terrorism.tsv";
+
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        data = CSVReader.Read(filename);
         points = new List<Point>();
+        pointObjects = new List<GameObject>();
         updateQueued = true;
+
+        AddPoints(data, "country_txt", "count", (e) => { return true; });
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //render pointerinos
+        if (updateQueued)
+        {
+            pointObjects.Clear();
 
-        updateQueued = false;
+            foreach (var p in points)
+            {
+                GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                obj.name = p.Aggregate;
+                obj.transform.SetParent(this.transform, false);
+                obj.transform.localScale = new Vector3(p.Size, p.Size, p.Size * 4);
+                obj.GetComponent<MeshRenderer>().material.color = p.Color;
+
+                Vector3 position = new Vector3
+                {
+                    z = 0f,//Mathf.Sin(p.Position.y),
+                    x = Mathf.Sin(p.Position.x),// * Mathf.Cos(p.Position.y),
+                    y = Mathf.Cos(p.Position.x) //* Mathf.Cos(p.Position.y)
+                };
+                position *= 0.01f;
+                obj.transform.localPosition = position;
+                obj.transform.LookAt(2 * obj.transform.position - transform.position);
+                
+
+                pointObjects.Add(obj);
+            }
+            updateQueued = false;
+        }
 	}
 
-    public void AddPoint(Point newPoint)
+    public void AddPoints(List<Event> d, string aggregate, string heightColumn, Func<Event, bool> filter, bool append = false)
     {
-        points.Add(newPoint);
+        if (!append)
+        {
+            points.Clear();
+        }
+        foreach (Event e in d)
+        {
+            if (filter(e))
+            {
+                int i = points.FindIndex(p => p.Aggregate == e.values[aggregate]);
+                if (i < 0) 
+                {
+                    //new point
+                    points.Add(new Point
+                    {
+                        EventID = new List<string> { e.values["eventid"] },
+                        Aggregate = e.values[aggregate],
+                        Position = new Vector2(float.Parse(e.values["longitude"]), float.Parse(e.values["latitude"])),
+                        Color = Color.gray,
+                        Size = 0.001f,
+                        Height = heightColumn == "count" ? 1 : float.Parse(e.values[heightColumn])
+                    });
+                }
+                else
+                {
+                    //add to ith point
+                    points[i].EventID.Add(e.values["eventid"]);
+                    points[i].Position += new Vector2(float.Parse(e.values["longitude"]), float.Parse(e.values["latitude"]));
+                    points[i].Height += heightColumn == "count" ? 1 : float.Parse(e.values[heightColumn]);
+                }
+            }
+        }
+        //calc avg height and position
+        float maxHeight = 0;
+        foreach (Point p in points)
+        {
+            p.Position /= p.EventID.Count;
+            p.Height /= p.EventID.Count;
+            maxHeight = p.Height > maxHeight ? p.Height : maxHeight;
+        }
+        //normalize height to 1;
+        foreach (Point p in points)
+        {
+            p.Height /= maxHeight;
+        }
         updateQueued = true;
     }
-
+        
     public void ClearPoints()
     {
         points.Clear();
-        updateQueued = true;
-    }
-
-    public void AddPoints(List<Point> newPoints)
-    {
-        points.AddRange(newPoints);
         updateQueued = true;
     }
     
@@ -42,21 +117,10 @@ public class GlobeRender : MonoBehaviour {
 
 public class Point
 {
+    public List<string> EventID { get; set; }
+    public string Aggregate { get; set; }
     public Vector2 Position { get; set; }
     public Color Color { get; set; }
-    public float Radius { get; set; }
-
-    public Point()
-    {
-        Position = new Vector2();
-        Color = new Color();
-        Radius = 0.01f;
-    }
-
-    public Point(Vector2 position, Color color, float radius)
-    {
-        Position = position;
-        Color = color;
-        Radius = radius;
-    }
+    public float Size { get; set; }
+    public float Height { get; set; }
 }
